66563f29b53780d2a6f55ab1dfe61f4b6fd0029d
Kolawole Ojo <philliphopeojo@gmail.com>
2020-08-30 03:03:58 +0100

    Setting goals

---
+++ ciph-deciph.py
@@ -0,0 +1,9 @@
+'''
+1. Ciphering in Unicode Characters
+2. Ciphering in Base64
+3. Ciphering in Base32
+4. Ciphering in ASCII
+
+5. Detecting which mode the project was ciphered in and deciphering using the language decipherer.
+
+'''
